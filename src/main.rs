dc21f0ae0f56cc70316ea02ad472f5dd72546cb7
Saurabh Pahwa <saurabhpahwa31@gmail.com>
2023-12-07 19:21:22 -0800

    day7

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use crate::helpers::{read_input, read_new};
 use std::collections::HashMap;
+use std::ops::Index;
+use std::{cmp::Ordering, iter::zip};
 mod helpers {
     mod reader;
     pub use reader::read_input;
     pub use reader::read_new;
 }
-use itertools::Itertools;
+use itertools::{all, Itertools};
 use regex::Regex;
 
 fn day1(input: Vec<String>) {
@@ -446,25 +448,158 @@ fn day6() {
     }
 
     dbg!(way_c);
-
-    // let mut ways: Vec<i32> = Vec::new();
-    // for (index, time) in time.iter().enumerate() {
-    //     let mut way_c = 0;
-    //     for i in 0..*time {
-    //         //speed = i
-    //         let speed = i;
-    //         let rem_time = time - i;
-
-    //         let dist = speed * rem_time;
-
-    //         if dist > distance[index] {
-    //             way_c += 1;
-    //         }
-    //     }
-    //     ways.push(way_c);
-    // }
-    // dbg!(ways.iter().product::<i32>());
 }
+
+// Five of a kind, where all five cards have the same label: AAAAA
+// Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+// Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+// Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+// Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+// One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+// High card, where all cards' labels are distinct: 23456
+fn day7() {
+    #[derive(PartialEq, PartialOrd, Eq, Hash, Copy, Clone, Debug, Ord)]
+    enum CardType {
+        HighCard,
+        OnePair,
+        TwoPair,
+        ThreeOfaKind,
+        FullHouse,
+        FourOfaKind,
+        FiveOfaKind,
+    }
+
+    #[derive(Debug, Eq, Ord)]
+    struct Card<'a> {
+        card_type: CardType,
+        val: &'a str,
+        bid: i32,
+        ranks: [i32; 5],
+    }
+    impl PartialOrd for Card<'_> {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            if self.card_type != other.card_type {
+                return Some(self.card_type.cmp(&other.card_type));
+            } else {
+                // walk the ranks list
+                for i in 0..5 {
+                    if self.ranks[i] > other.ranks[i] {
+                        return Some(Ordering::Greater);
+                    } else if self.ranks[i] < other.ranks[i] {
+                        return Some(Ordering::Less);
+                    } else {
+                        continue;
+                    }
+                }
+
+                Some(Ordering::Equal)
+            }
+        }
+    }
+
+    impl PartialEq for Card<'_> {
+        fn eq(&self, other: &Self) -> bool {
+            self.val == other.val
+        }
+    }
+
+    fn get_ctype(val: &str) -> CardType {
+        let mut counts: HashMap<char, i32> = HashMap::new();
+        let c_type: CardType;
+        let rank: Vec<i32> = Vec::new();
+
+        for x in val.chars() {
+            *counts.entry(x).or_default() += 1;
+        }
+        let x: Vec<i32> = counts.values().cloned().sorted().collect_vec();
+
+        if x == vec![1, 1, 1, 1, 1] {
+            CardType::HighCard
+        } else if x == vec![1, 1, 1, 2] {
+            CardType::OnePair
+        } else if x == vec![1, 2, 2] {
+            CardType::TwoPair
+        } else if x == vec![1, 1, 3] {
+            CardType::ThreeOfaKind
+        } else if x == vec![2, 3] {
+            CardType::FullHouse
+        } else if x == vec![1, 4] {
+            CardType::FourOfaKind
+        } else {
+            CardType::FiveOfaKind
+        }
+    }
+
+    let inputs = read_input("inputs/day7.txt".to_string());
+    let ranks = [
+        'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
+    ];
+    let cards: Vec<(&str, &str)> = inputs.iter().map(|x| x.split_once(" ").unwrap()).collect();
+    let mut c: Vec<Card> = Vec::new();
+    for (val, bid) in cards {
+        //figure  out card type
+        // let counts =  v.iter().filter(|&n| *n == 91).count();
+
+        let mut c_type: CardType;
+
+        if val.contains('J') {
+            //make new card
+
+            let mut tmp_card = val;
+            let mut all_ctypes: Vec<CardType> = Vec::new();
+            let j_count = tmp_card.chars().filter(|&n| n == 'J').count();
+            if j_count >= 4 {
+                c_type = CardType::FiveOfaKind
+            } else {
+                let all_perms = itertools::repeat_n(ranks, j_count).multi_cartesian_product();
+                let j_indices = tmp_card
+                    .chars()
+                    .enumerate()
+                    .filter(|(_, r)| *r == 'J')
+                    .map(|(index, _)| index)
+                    .collect::<Vec<_>>();
+
+                for perm in all_perms {
+                    let mut inter_val = tmp_card.chars().collect_vec();
+                    for (j_ind, p) in zip(&j_indices, perm) {
+                        inter_val[*j_ind] = p;
+                    }
+                    let istr: String = inter_val.iter().collect();
+
+                    all_ctypes.push(get_ctype(&istr));
+                }
+
+                all_ctypes.sort();
+                all_ctypes.reverse();
+                c_type = all_ctypes[0];
+            }
+        } else {
+            c_type = get_ctype(val);
+        }
+
+        c.push(Card {
+            card_type: c_type,
+            val: val,
+            bid: bid.parse().unwrap(),
+            ranks: val
+                .chars()
+                .map(|x| ranks.iter().position(|&r| r == x).unwrap() as i32)
+                .collect::<Vec<i32>>()
+                .try_into()
+                .unwrap(),
+        })
+    }
+    //sort by CardType
+    c.sort();
+    println!(
+        "Answer: {}",
+        c.iter()
+            .enumerate()
+            .map(|(idx, x)| x.bid * (idx + 1) as i32)
+            .sum::<i32>()
+    );
+}
+
 fn main() {
     // day1two();
     // day2();
@@ -472,4 +607,5 @@ fn main() {
     // day4();
     // day5();
     // day6();
+    // day7();
 }
